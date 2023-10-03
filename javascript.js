bf188966ebf8501f4765de28ab17ca9eeac0c927
Cowlover3000 <niels851@hotmail.com>
2023-10-03 18:45:07 +0200

    kept working on the grid added the button

--- javascript.js
+++ javascript.js
@@ -1,12 +1,24 @@
-function createDivs() {
+
+
+
+// generates a grid of divs that change color when you hover over them for a variable num which represent the with and height
+
+function createDivs(num) {
     const container = document.querySelector('#container')
-    for (let index = 0; index < 16; index++) {
-        for (let j = 0; j < 16; j++) {
+    for (let index = 0; index < num; index++) {
+        for (let j = 0; j < num; j++) {
             let div = document.createElement('div');
             div.classList.add('grid-element')
+            div.style.cssText = `background-color: purple; width: calc(480px / ${num}); height: calc(480px / ${num})`;
+            div.addEventListener('mouseover', (event) => {
+                event.target.style['background-color'] = 'blue';
+            })
+
             container.appendChild(div);    
         }
     }
 }
 
-createDivs()
+
+
+createDivs(64)
