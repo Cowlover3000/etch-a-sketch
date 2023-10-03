af6fed28f1e3588b2dcfb04132e1e22fe324a694
Cowlover3000 <niels851@hotmail.com>
2023-10-03 19:35:15 +0200

    added the option to resize the grid and the previous grid now gets removed

--- javascript.js
+++ javascript.js
@@ -1,10 +1,31 @@
+let btn = document.querySelector('button');
 
+btn.addEventListener('click', () => {
+    let whileRunning = true;
+    let num;
+    while (whileRunning) {
+        num =  prompt("Give a number between 1 - 100", "42");
+
+        if (parseInt(num) >= 1  && parseInt(num) <= 100) {
+            whileRunning = false;
+            createDivs(parseInt(num));
+        }
+    }
+})
 
 
 // generates a grid of divs that change color when you hover over them for a variable num which represent the with and height
 
 function createDivs(num) {
     const container = document.querySelector('#container')
+
+    if (container.hasChildNodes()) {
+        let childElements = Array.from(container.children);
+        childElements.forEach(element => {
+            element.remove();
+        });
+    }
+
     for (let index = 0; index < num; index++) {
         for (let j = 0; j < num; j++) {
             let div = document.createElement('div');
